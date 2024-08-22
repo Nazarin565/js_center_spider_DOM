36401841bcae7afcaca5ff48cdb2a7a553a385ca
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-08-22 21:42:44 +0300

    make build

---
+++ index.7f649921.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");t.style.position="relative",e.style.position="absolute",e.style.left="".concat(t.clientHeight/2-e.clientHeight/2,"px"),e.style.top="".concat(t.clientWidth/2-e.clientWidth/2,"px");
+//# sourceMappingURL=index.7f649921.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+wall.style.position = "relative";
+spider.style.position = "absolute";
+spider.style.left = `${wall.clientHeight / 2 - spider.clientHeight / 2}px`;
+spider.style.top = `${wall.clientWidth / 2 - spider.clientWidth / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
